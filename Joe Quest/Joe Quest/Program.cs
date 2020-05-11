a30a76c208b3abaf166cd160174367cc2e548f76
Falk Burger <falkburger1@gmail.com>
2020-05-11 14:34:34 +0200

    I think this fixes it

--- Joe Quest/Joe Quest/Program.cs
+++ Joe Quest/Joe Quest/Program.cs
@@ -29,19 +29,18 @@ namespace Joe_Quest
             Console.WriteLine("Hello World! This is Joe Quest, an experiment of my programming skills to create a basic text adventure. ");
             Console.WriteLine("\nYou are Joe, an average guy. You have found a great castle with many potential treasures. Before you stands this mighty castle and it's" +
                 " wooden door with a lever and a jewel atop the door. Behind you is the" +
-                " dense forest from whence you came. There is no other way to go than north.\n");
+                " dense forest from whence you came. There is no other way to go than north.");
 
             do
             {
                 input = Console.ReadLine();
-                var segs = input.Split(' ');
 
                 if (input == "exit")
                 {
                     MainObj.exit = true;
                 }
 
-                else if (segs[0] == "move")
+                else if (LikeOperator.LikeString(input, "move*", Microsoft.VisualBasic.CompareMethod.Binary))
                 {
                     CommandObj.Move(Convert.ToInt32(File.ReadAllText("RoomDetail.txt")), input.Substring(input.IndexOf(" ") + 1));
                     //copy text after move 
@@ -49,12 +48,12 @@ namespace Joe_Quest
 
                 }
 
-                else if (segs[0] == "look")
+                else if (LikeOperator.LikeString(input, "look*", Microsoft.VisualBasic.CompareMethod.Binary))
                 {
                     CommandObj.Look(Convert.ToInt32(File.ReadAllText("RoomDetail.txt")), input.Substring(input.IndexOf(" ") + 1));
                 }
 
-                else if (segs[0] == "take")
+                else if (LikeOperator.LikeString(input, "take*", Microsoft.VisualBasic.CompareMethod.Binary))
                 {
                     CommandObj.Take(Convert.ToInt32(File.ReadAllText("RoomDetail.txt")), input.Substring(input.IndexOf(" ") + 1));
                 }
